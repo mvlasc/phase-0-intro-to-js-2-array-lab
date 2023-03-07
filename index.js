7767f8a3736d4346ec2c4c007530d5cea1217297
marissa <marissavelasco2@outlook.com>
2023-03-06 20:25:20 -0700

    short message briefly explaining the changes saved in this commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return "Ralph"
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return "Bob"
+
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return 'it removes the last cat from the array'
+
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo");
+    return 'it removes the first cat from the array'
+}
+function appendCat(name) { 
+    const newArray = cats.slice();
+    newArray.push("Broom")
+    return newArray
+}
+function prependCat(name) {
+    const newPrependCat = cats.slice();
+    newPrependCat.unshift("Arnold")
+    return newPrependCat
+}
+function removeLastCat() {
+    const newRemoveLastCat = cats.slice();
+    newRemoveLastCat.pop()
+    return newRemoveLastCat
+}
+function removeFirstCat() {
+   const newRemoveFirstCat = cats.slice();
+   newRemoveFirstCat.shift()
+    cats.splice();
+    return newRemoveFirstCat
+}
+
+
